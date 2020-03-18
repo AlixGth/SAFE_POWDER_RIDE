91d92ed9f3f6f83feec62cc306a38a85c1785efb
Jerome Crest <jerome.crest@gmail.com>
2020-03-18 15:34:41 +0100

    final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   def user_not_authorized
      flash[:alert] = "You are not authorized to perform this action."
-     redirect_to(root_path)
+     redirect_to(home_path)
   end
 
   private
