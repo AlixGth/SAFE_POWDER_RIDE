5a44314b2b062b8d397e479862c7802bd29fbe13
Jerome Crest <jerome.crest@gmail.com>
2020-03-19 13:04:05 +0100

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :itineraries, only: [:index, :show, :new, :create]
   get '/mon-compte', to: "pages#my_account"
+  get '/concept', to: "pages#concept"
 
   authenticate :user, lambda { |u| u.admin } do
     mount Sidekiq::Web => '/sidekiq'
