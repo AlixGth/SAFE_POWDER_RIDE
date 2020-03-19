90fd100c7a875b104cd288e0410dae6b43ccd199
Gaetan Mallet <mallet.gaetan@hotmail.fr>
2020-03-19 11:49:35 +0100

    Creation and show of intinerary w/ map

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -35,12 +35,13 @@ class ItinerariesController < ApplicationController
         array = array.select.with_index do |coordinate, index|
           index % reduce_value == 0
         end
-        for i in (1...array.size)
-          Coordinate.create(longitude: array[i][0].to_f, latitude: array[i][1].to_f, color: nil, order: i, itinerary: @itinerary)
-          if i % 4 == 0
+        for i in (0...array.size)
+          if i != 0 && i % 4 == 0
             color = array[i-1][2].to_i > 1500 ? "#F71F0A" : "#F7B20A"
             array[i] = [array[i][0], array[i][1], color]
             Coordinate.create(longitude: array[i][0].to_f, latitude: array[i][1].to_f, color: color, order: i, itinerary: @itinerary)
+          else
+            Coordinate.create(longitude: array[i][0].to_f, latitude: array[i][1].to_f, color: nil, order: i, itinerary: @itinerary)
           end
         end
       else
