bf1075f5e271b6adbb88d9cafdf04853f8f5e8a0
Thibault Adet <thibault.adet@gmail.com>
2020-03-20 15:56:18 +0100

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -146,9 +146,9 @@ def coordinates(filename, itinerary)
 	  if i != 0 && i % 4 == 0
 	    color = array[i-1][2].to_i > 1500 ? "#F71F0A" : "#F7B20A"
 	    array[i] = [array[i][0], array[i][1], color]
-	    Coordinate.create(longitude: array[i][0].to_f, latitude: array[i][1].to_f, color: color, order: i, itinerary: itinerary)
+	    Coordinate.create(longitude: array[i][0].to_f, latitude: array[i][1].to_f, altitude: array[i][2].to_f, color: color, order: i, itinerary: itinerary)
 	  else
-	    Coordinate.create(longitude: array[i][0].to_f, latitude: array[i][1].to_f, color: nil, order: i, itinerary: itinerary)
+	    Coordinate.create(longitude: array[i][0].to_f, latitude: array[i][1].to_f, altitude: array[i][2].to_f, color: nil, order: i, itinerary: itinerary)
 	  end
 	end
 end
