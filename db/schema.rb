0b38190f345b986d17d2a9b77be641644a49d06a
Thibault Adet <thibault.adet@gmail.com>
2020-03-23 11:36:28 +0100

    migration add evol color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_170102) do
+ActiveRecord::Schema.define(version: 2020_03_23_103539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_03_18_170102) do
     t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "evol_color"
     t.index ["itinerary_id"], name: "index_coordinates_on_itinerary_id"
   end
 
