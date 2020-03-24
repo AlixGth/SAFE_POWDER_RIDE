c9f527102a0b285fd35bf440431e4cc367d3375f
Thibault Adet <thibault.adet@gmail.com>
2020-03-24 23:23:05 +0100

    add meta tag on show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
 import "bootstrap";
 import { displayRoute } from '../plugins/displaySegment';
 import { fixFooter } from '../plugins/footer';
+import { navbarShow } from '../plugins/navbarShow';
 import { navbar } from '../plugins/navbar';
 
 fixFooter();
 displayRoute();
 navbar();
+navbarShow();
--- app/javascript/plugins/navbar.js
+++ app/javascript/plugins/navbar.js
@@ -2,34 +2,9 @@ const navbar = () => {
   const navbar = document.getElementById('navbar');
   const toggle = document.getElementById('toggle');
   const toggleIcon = document.getElementById('menu-icon');
-  console.log(document);
-  document.addEventListener("scroll", (e) => {
-    const map = document.getElementById("map");
-    if (map) {
-      if (navbar.offsetHeight < window.scrollY) {
-        // navbar.style.transition = "all 0.3s";
-        navbar.classList.remove('naked-navbar');
-        navbar.style.position = 'fixed';
-        navbar.style.width = '100%';
-      } else {
-        navbar.classList.add('naked-navbar');
-      }
-    }
-  });
-  toggle.addEventListener('click', (e) => {
-    if (toggle.getAttribute('aria-expanded') === 'false') {
-      // navbar.style.transition = "all 0.3s";
-      navbar.classList.remove('naked-navbar');
-      navbar.style.position = 'fixed';
-      navbar.style.width = '100%';
-      toggleIcon.classList.add('fa-times');
-      toggleIcon.classList.remove('fa-bars');
-    } else {
-      navbar.classList.add('naked-navbar');
-      toggleIcon.classList.add('fa-bars');
-      toggleIcon.classList.remove('fa-times');
-    }
-  });
+  console.log(navbar)
+  console.log(toggle)
+  console.log(toggleIcon)
 };
 
 export { navbar };
