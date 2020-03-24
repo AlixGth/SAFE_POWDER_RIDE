d5bf27c7272a079916850d141dcb81e8cc98c764
Jerome Crest <jerome.crest@gmail.com>
2020-03-24 11:57:02 +0100

    done my_favorites page without email view and pdf

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,7 @@ class FavoritesController < ApplicationController
 
   def index
     @favorites = policy_scope(Favorite)
+    @colors = {"1" => "#CAFF66", "2" => "#FBFF01", "3" => "#FE9800", "4" => "#FD0200", "5" => "#CB0200"}
   end
 
   def create
@@ -10,5 +11,12 @@ class FavoritesController < ApplicationController
     @favorite.user = current_user
     @favorite.itinerary = Itinerary.find(params[:itinerary_id])
     @favorite.save
+  end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    authorize(@favorite)
+    @favorite.destroy
+    redirect_to(favorites_path)
   end
 end
--- app/policies/favorite_policy.rb
+++ app/policies/favorite_policy.rb
@@ -8,4 +8,8 @@ class FavoritePolicy < ApplicationPolicy
   def create?
     true
   end
+
+  def destroy?
+    record.user == user
+  end
 end
